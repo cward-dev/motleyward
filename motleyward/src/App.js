f5741f41c9578744361e3a58141057af3e31b1a5
Chris Ward <cshonward@gmail.com>
2022-02-19 00:21:14 -0600

    push

--- motleyward/src/App.js
+++ motleyward/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './motleyward-logo-1000.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCg67HdKbssS3UYehoqCwlmw"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          YouTube
         </a>
       </header>
     </div>
